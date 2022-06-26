37972baae0e0e300d2f0fe126f59d54ef926a6f2
Mark Rogers <wpgitpro@outlook.com>
2022-06-25 22:16:42 -0500

    v0.1 changes

--- hello.f90
+++ hello.f90
@@ -1,10 +1,16 @@
+! Version: 0.1
+!
 PROGRAM helloworld
 USE mathModule
 IMPLICIT NONE
+INTEGER nl
 COMPLEX :: link
 link = (3,4)
 PRINT *, "Hello World!"  
 PRINT *, "pi:", pi, "e:", e, "gamma:", gamma  
 PRINT *, link
+WRITE(*,*) "Number Of Mechanism Loops ?"
+READ(*,'(I1)') nl
+WRITE(*,*) nl
 STOP 0 
 END PROGRAM helloworld
