ab08814aacdc414e0ed8200826754765aceeeeb8
Mark Rogers <wpgitpro@outlook.com>
2022-07-15 09:15:33 -0500

    Update v0.1.2 with model storage

--- hello.f90
+++ hello.f90
@@ -9,6 +9,7 @@ INTEGER :: nl
 INTEGER, DIMENSION(5) :: nv
 COMPLEX :: link, linka, linkb
 REAL, DIMENSION(5,5) :: model
+LOGICAL :: lexist
 linka = (3,4)
 linkb = (4,6)
 link = linka + linkb
@@ -21,7 +22,7 @@ WRITE(*,*) nl
 WRITE(*,*) "Maximum number of vectors in any loop ?"
 READ(*,'(I1)') 
 do i = 1, nl
-    WRITE(*,*) "Number of Links in Loop ?"
+    WRITE(*,*) "Number of Links in Loop ?", i
     READ(*,'(I1)') nv(i)
     do j = 1, nv(i)
         WRITE(*,*) "Length of link ", j, "?"
@@ -29,6 +30,33 @@ do i = 1, nl
         WRITE(*,*) "Link ", j, " is ", model(i,j)
     end do
 end do
+!
+! Does model file already exist?
+INQUIRE(FILE="model.dat",EXIST=lexist)
+IF (.NOT. lexist) THEN
+    OPEN(UNIT=9,FILE='model.dat',STATUS='NEW',ACTION='WRITE')
+    ! Output
+    ! Number of vectors in get_model
+    ! Number of Loops
+    ! Number of vectors in loop
+    WRITE(9,*) nv(1), nl, nv(1)
+    CLOSE(9,STATUS='KEEP')
+END IF
+!
 WRITE(*,*) "End of program"
 STOP 0 
 END PROGRAM helloworld
+
+SUBROUTINE save_model(model)
+    ! notes
+END SUBROUTINE save_model
+
+SUBROUTINE get_model(model)
+    ! notes
+END SUBROUTINE get_model
+
+SUBROUTINE check_model(model)
+    ! notes
+END SUBROUTINE check_model
+
+
