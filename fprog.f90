1b799c52f806fd1a78bf59cada9936ba821c1145
Mark Rogers <wpgitpro@outlook.com>
2023-12-04 09:23:13 -0600

    Update fprog.f90

--- fprog.f90
+++ fprog.f90
@@ -1,11 +1,27 @@
 program f_prog
-    integer :: i,j(2)
+    integer :: i
     real :: r
     character(len=10) :: str
-
-    i = 4711
-    j = (/13, 14/)
-    r = 4712.0
-    str = "abcd"
-    call c_sub(trim(str)//char(0), i, r, j)
+    common i, r
+    i = 111
+    r = 112.0
+    str = "efghij"
+    write(*,'(A,F5.1)') "This is the real: ", r
+    call c_sub(trim(str)//char(0), i, r)
+    call testsub()
+    write(*,'(A,F5.1)') "This is the real: ", r
 end program f_prog
+
+!
+! gcc -c -fno-leading-underscore csub.c
+!
+
+subroutine testsub()
+integer i 
+real r 
+common i, r 
+r = 211.0
+write(*,'(A,I3)') "This is the integer: ", i
+write(*,'(A,F5.1)') "This is the real: ", r
+end subroutine testsub
+
